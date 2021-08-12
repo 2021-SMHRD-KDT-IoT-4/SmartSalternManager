31198c81412483bf1403e95029a025356f8f88f3
smhrd <smhrd@DESKTOP-6IHAUI2>
2021-08-12 14:48:49 +0900

    .

---
+++ github.com/2021-SMHRD-KDT-IoT-4/porkbeef-.git/Project/src/main/java/com/Controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginController implements Command {
+
+	@Override
+	public void command(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
